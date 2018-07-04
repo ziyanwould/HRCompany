29b04c85351386b440e4c59fb07c2bc514241845
ziyanwould <448736378@qq.com>
2018-07-04 17:55:30 +0800

    建构的提交

---
+++ pages/index/index.js
@@ -0,0 +1,68 @@
+
+
+Page({
+  data: {
+    //自定义模板必须引入的数据版块
+    navData: [
+      {
+        name: "兼职人才",  //文本
+        current: 1,    //是否是当前页，0不是  1是
+        style: 0,     //样式
+        ico: '.icon-fabu',  //不同图标
+        fn: 'gotoCompanyIndex'   //对应处理函数
+      }, {
+        name: "全职人才",
+        current: 0,
+        style: 0,
+        ico: 'icon-mingpianjia',
+        fn: 'gotobusinessCard'
+      }, {
+        name: "发布",
+        current: 0,
+        style: 1,
+        ico: '',
+        fn: 'gotopublish'
+      }, {
+        name: "消息",
+        current: 0,
+        style: 0,
+        ico: 'icon-yikeappshouyetubiao35',
+        fn: 'gotoMessages'
+      }, {
+        name: "公司",
+        current: 0,
+        style: 0,
+        ico: 'icon-wode',
+        fn: 'bindViewMy'
+      },
+    ],
+
+  },
+  gotoCompanyIndex:function(){
+    wx.reLaunch({
+      url: '/pages/index/index'
+    })
+  },
+  gotobusinessCard:function(){
+    wx.reLaunch({
+      url: '/pages/detail/detail'
+    })
+  },
+  gotopublish: function () {
+    wx.reLaunch({
+      url: '/pages/creation/creation'
+    })
+  },
+  gotoMessages: function () {
+    wx.reLaunch({
+      url: '/pages/news/news'
+    })
+  },
+  bindViewMy: function () {
+    wx.reLaunch({
+      url: '/pages/user/user'
+    })
+  },
+
+
+})
